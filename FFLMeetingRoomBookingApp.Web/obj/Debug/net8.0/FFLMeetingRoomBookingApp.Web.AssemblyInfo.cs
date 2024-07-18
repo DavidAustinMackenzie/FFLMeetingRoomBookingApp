f071fa2c67ff277f56cd8146844a5212a9e65b61
Dave <dmackenzie1985@gmail.com>
2024-07-18 17:29:50 +1000

    Formatted buttons on all pages

--- FFLMeetingRoomBookingApp.Web/obj/Debug/net8.0/FFLMeetingRoomBookingApp.Web.AssemblyInfo.cs
+++ FFLMeetingRoomBookingApp.Web/obj/Debug/net8.0/FFLMeetingRoomBookingApp.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FFLMeetingRoomBookingApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5abad623e0b6e8540f72887912a887b9b4255f19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a3735f6da46f4b65659c633315d7a8928e2841")]
 [assembly: System.Reflection.AssemblyProductAttribute("FFLMeetingRoomBookingApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FFLMeetingRoomBookingApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
